4646dcac1ac5a32aeae8aa783049ed906fbd15df
Jeffrey Jen <yihungjen@gmail.com>
2019-11-26 17:22:54 +0800

    Fix missing concrete content type inline formset save

--- django/contrib/contenttypes/forms.py
+++ django/contrib/contenttypes/forms.py
@@ -44,7 +44,7 @@ class BaseGenericInlineFormSet(BaseModelFormSet):
         )
 
     def save_new(self, form, commit=True):
-        setattr(form.instance, self.ct_field.get_attname(), ContentType.objects.get_for_model(self.instance).pk)
+        setattr(form.instance, self.ct_field.get_attname(), ContentType.objects.get_for_model(self.instance, for_concrete_model=self.for_concrete_model).pk)
         setattr(form.instance, self.ct_fk_field.get_attname(), self.instance.pk)
         return form.save(commit=commit)
 
